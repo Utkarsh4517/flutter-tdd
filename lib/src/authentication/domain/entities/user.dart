38097347de5b50516465f2ff6392b36fb1a85b6c
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-03-02 02:22:46 +0530

    created user entity

---
+++ lib/src/authentication/domain/entities/user.dart
@@ -0,0 +1,19 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  const User({
+    required this.id,
+    required this.createdAt,
+    required this.name,
+    required this.avatar,
+  });
+
+  final int id;
+  final String createdAt;
+  final String name;
+  final String avatar;
+
+  // Equatable package is used to equate two objects if they have have the same id, irrespective of their other values such as name.
+  @override
+  List<Object?> get props => [id];
+}
